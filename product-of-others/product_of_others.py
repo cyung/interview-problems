9589ac2272dff6a36fad06246756c262b5b91ab4
Christopher Yung <cyung10892@gmail.com>
2016-02-16 15:39:13 -0500

    reduce space complexity by n for product-of-others

--- product-of-others/product_of_others.py
+++ product-of-others/product_of_others.py
@@ -1,25 +1,23 @@
-def get_other_products(num_list):
-  running_products = []
-  products_before_index = [None] * len(num_list)
-  products_after_index = [None] * len(num_list)
+def product_of_others(num_list):
+  products_before_index = []
+  products_except_at_index = [None] * len(num_list)
 
   current_product_before = 1
   current_product_after = 1
 
-  for idx, val in enumerate(num_list):
-    index_after = len(num_list)-1-idx
-    products_before_index[idx] = current_product_before
-    products_after_index[index_after] = current_product_after
-    current_product_before *= val
-    current_product_after *= num_list[index_after]
+  for num in num_list:
+    products_before_index.append(current_product_before)
+    current_product_before *= num
 
-  products = []
-  for i in range(len(num_list)):
-    products.append(products_before_index[i]*products_after_index[i])
+  i = len(num_list) - 1
+  while i >= 0:
+    products_except_at_index[i] = products_before_index[i] * current_product_after
+    current_product_after *= num_list[i]
+    i -= 1
 
-  return products
+  return products_except_at_index
 
 if __name__ == "__main__":
   num_list = [2,3,4,5]
 
-  print(get_other_products(num_list))
+  print(product_of_others(num_list))
