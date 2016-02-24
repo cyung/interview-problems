0fdc12ade58bec997fd968dac2cc7c9a07cba43f
Christopher Yung <cyung10892@gmail.com>
2016-02-23 22:48:10 -0500

    add layout for java solution for largestProduct

---
+++ largest-product/largestProduct.java
@@ -0,0 +1,14 @@
+
+public class largestProduct {
+
+  public static void main(String[] args) {
+    int[] numList = new int[]{-7, -2, 3, 10, 3};
+    
+    System.out.println(largestProduct(numList));
+  }
+
+  public static int largestProduct(int[] numList) {
+    
+  }
+
+}
